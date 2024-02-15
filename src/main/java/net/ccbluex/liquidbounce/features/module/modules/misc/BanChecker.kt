9e285ee5953faf65cad313f6df362101ee3b15cd
CatX_feitu <3205711786@qq.com>
2024-02-15 18:18:31 +0800

    [#] 修改 PridePlus 的 BanChecker

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/BanChecker.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/BanChecker.kt
@@ -6,26 +6,35 @@ import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.features.value.BoolValue
+import net.ccbluex.liquidbounce.features.value.ListValue
+import net.ccbluex.liquidbounce.features.value.TextValue
 import net.ccbluex.liquidbounce.ui.client.hud.element.elements.Notification
 import net.ccbluex.liquidbounce.ui.client.hud.element.elements.NotifyType
 import net.minecraft.network.play.server.SPacketChat
 import java.util.regex.Pattern
 
-@ModuleInfo(name = "BanChecker", description = "For HuaYuTing", category = ModuleCategory.MISC)
+@ModuleInfo(name = "BanChecker", description = "Test ban player(s) and autoL", category = ModuleCategory.MISC)
 class BanChecker : Module(){
+    private val banMessage = TextValue("BanMessage","玩家(.*?)在本局游戏中行为异常")
+    private val autoL = BoolValue("AutoL",false)
 
     var ban = 0
     @EventTarget
     fun onPacket(event : PacketEvent){
         val packet = event.packet
         if(packet is SPacketChat){
-            val matcher = Pattern.compile("玩家(.*?)在本局游戏中行为异常").matcher(packet.chatComponent.unformattedText)
+            val matcher = Pattern.compile(banMessage.get()).matcher(packet.chatComponent.unformattedText)
             if(matcher.find()){
                 ban ++
-                val banname = matcher.group(1)
-                DarkNya.hud.addNotification(Notification("BanChecker","$banname was banned. (banned:$ban)",NotifyType.INFO, animeTime = 1000))
-                mc.player!!.sendChatMessage("@ $banname  主播你怎么ban了啊 这都ban了" + ban + "个人了 ")
+                val banned = matcher.group(1)
+                if (banned == "") return
+                if (autoL.get()) mc.player!!.sendChatMessage("@ $banned  主播你怎么ban了啊 这都ban了" + ban + "个人了 ")
+
+                DarkNya.hud.addNotification(Notification("BanChecker","$banned was banned. (banned:$ban)",NotifyType.INFO, animeTime = 1000))
             }
         }
     }
+    override val tag: String
+        get() = ban.toString()
 }
