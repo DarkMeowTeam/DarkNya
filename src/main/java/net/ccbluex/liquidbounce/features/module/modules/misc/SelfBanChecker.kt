cfc59d98e2d42a6eeb9734cac3575ea9f500f50d
CatX_feitu <3205711786@qq.com>
2024-02-15 18:19:51 +0800

    [+] SelfBanChecker - 当检测到自己被服务器ban掉后可以执行一些操作

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/SelfBanChecker.kt
@@ -0,0 +1,59 @@
+package net.ccbluex.liquidbounce.features.module.modules.misc
+
+import net.ccbluex.liquidbounce.DarkNya
+import net.ccbluex.liquidbounce.event.EventTarget
+import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+import net.ccbluex.liquidbounce.features.module.modules.combat.KillAura
+import net.ccbluex.liquidbounce.features.module.modules.player.Blink
+import net.ccbluex.liquidbounce.features.value.BoolValue
+import net.ccbluex.liquidbounce.features.value.ListValue
+import net.ccbluex.liquidbounce.features.value.TextValue
+import net.minecraft.network.play.server.SPacketChat
+import java.util.regex.Pattern
+
+/**
+ * Skid or Made By CatX_feitu
+ * @date 2024/2/15 15:14
+ * @author WaWa
+ */
+@ModuleInfo(name = "SelfBnChecker", category =ModuleCategory.MISC, description = "HytSelf ban checker")
+class SelfBanChecker : Module() {
+    private val server = ListValue("Server",arrayOf("Hyt"),"Hyt")
+    private val actionSendMessage = BoolValue("SendMessage",false)
+    private val actionSendMessageContent = TextValue("SendMessageContent","")
+    private val actionSendQuittingDisconnectingPacket = BoolValue("sendQuittingDisconnectingPacket",false)
+    private val actionDisableKillauraBlink = BoolValue("DisableKillauraBlink",true)
+    private var banCount = 0
+
+    override fun onEnable() {
+        banCount = 0
+    }
+
+
+    @EventTarget
+    fun onPacket(event : PacketEvent){
+        val packet = event.packet
+        if(packet is SPacketChat){
+            if (server.get() == "Hyt") {
+                val matcher =
+                    Pattern.compile("玩家${mc.player?.name}在本局游戏中行为异常").matcher(packet.chatComponent.unformattedText)
+                if (matcher.find()) {
+                    onBan()
+                }
+            }
+        }
+    }
+
+    private fun onBan() {
+        if (actionSendMessage.get() && actionSendMessageContent.get() != "") mc.player?.sendChatMessage(actionSendMessageContent.get())
+        if (actionSendQuittingDisconnectingPacket.get()) mc.world?.sendQuittingDisconnectingPacket()
+        if (actionDisableKillauraBlink.get()) (DarkNya.moduleManager[Blink::class.java]).state = false ; (DarkNya.moduleManager[KillAura::class.java]).state = false
+        banCount ++
+    }
+
+    override val tag: String
+        get() = banCount.toString()
+}
