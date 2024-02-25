a076e20d543337de836fa9a253df241293943ee7
CatX_feitu <3205711786@qq.com>
2024-02-25 17:48:29 +0800

    delete: 删除 DonatorCape 模块

--- src/main/java/net/ccbluex/liquidbounce/DarkNya.kt
+++ src/main/java/net/ccbluex/liquidbounce/DarkNya.kt
@@ -7,7 +7,6 @@ import net.ccbluex.liquidbounce.features.command.CommandManager
 import net.ccbluex.liquidbounce.features.module.ModuleManager
 import net.ccbluex.liquidbounce.features.special.AntiForge
 import net.ccbluex.liquidbounce.features.special.BungeeCordSpoof
-import net.ccbluex.liquidbounce.features.special.DonatorCape
 import net.ccbluex.liquidbounce.file.FileManager
 import net.ccbluex.liquidbounce.script.ScriptManager
 import net.ccbluex.liquidbounce.script.remapper.Remapper.loadSrg
@@ -76,7 +75,6 @@ object DarkNya {
         eventManager.registerListener(RotationUtils())
         eventManager.registerListener(AntiForge())
         eventManager.registerListener(BungeeCordSpoof())
-        eventManager.registerListener(DonatorCape())
         eventManager.registerListener(InventoryUtils())
 
         tipSoundManager = TipSoundManager()
