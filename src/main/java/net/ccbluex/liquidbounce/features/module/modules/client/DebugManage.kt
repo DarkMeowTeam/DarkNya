9843de1eadca04ce270c49cf59367cb95bcbeed1
CatX_feitu <108512490+catx-feitu@users.noreply.github.com>
2024-03-07 20:45:22 +0800

    refactor(DebugManage): 选项

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/DebugManage.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/DebugManage.kt
@@ -5,23 +5,26 @@ import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.features.value.BoolValue
+import net.ccbluex.liquidbounce.features.value.ListValue
 import net.ccbluex.liquidbounce.utils.ClientUtils
 
 @ModuleInfo(name = "DebugManage", description = "控制全局模块输出debugger", category = ModuleCategory.CLIENT, array = false)
 class DebugManage : Module() {
-    val infoValue = BoolValue("Info", true)
-    val warnValue = BoolValue("Warn", true)
+    var optValue = ListValue("Opt",arrayOf("All","Less"),"Only")
 
     companion object {
         @JvmStatic
         fun info(info: String) {
-            if (DarkNya.moduleManager[DebugManage::class.java].state && (DarkNya.moduleManager[DebugManage::class.java] as DebugManage).infoValue.get()) {
+            if (
+                DarkNya.moduleManager[DebugManage::class.java].state  &&
+                (DarkNya.moduleManager[DebugManage::class.java] as DebugManage).optValue.get().toLowerCase() == "less"
+            ) {
                 ClientUtils.displayChatMessage("§d${DarkNya.CLIENT_NAME} §8>> §7$info")
             }
         }
         @JvmStatic
         fun warn(info: String) {
-            if (DarkNya.moduleManager[DebugManage::class.java].state && (DarkNya.moduleManager[DebugManage::class.java] as DebugManage).warnValue.get()) {
+            if (DarkNya.moduleManager[DebugManage::class.java].state) {
                 ClientUtils.displayChatMessage("§d${DarkNya.CLIENT_NAME} §c>> §7$info")
             }
         }
