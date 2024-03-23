7bb5fd71e834ac393b0dabcc410413dd71d79aa0
CatX_feitu <108512490+catx-feitu@users.noreply.github.com>
2024-03-23 14:07:59 +0800

    remove: ClickGUI v2

--- src/main/java/net/ccbluex/liquidbounce/ui/client/clickgui/newVer/extensions/AnimHelper.kt
+++
@@ -1,8 +0,0 @@
-package net.ccbluex.liquidbounce.ui.client.clickgui.newVer.extensions
-
-import net.ccbluex.liquidbounce.features.module.modules.client.ClickGUINew
-import net.ccbluex.liquidbounce.utils.render.RenderUtils
-import op.wawa.utils.animation.AnimationUtil
-
-fun Float.animSmooth(target: Float, speed: Float) = if (ClickGUINew.fastRenderValue.get()) target else AnimationUtil.animate(target, this, speed * RenderUtils.deltaTime * 0.025F)
-fun Float.animLinear(speed: Float, min: Float, max: Float) = if (ClickGUINew.fastRenderValue.get()) { if (speed < 0F) min else max } else (this + speed).coerceIn(min, max)
