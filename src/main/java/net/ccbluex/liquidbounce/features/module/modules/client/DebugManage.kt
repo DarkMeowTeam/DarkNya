c880a76e89ddf8e176cc81e705fb420ca2cff87a
CatX_feitu <108512490+catx-feitu@users.noreply.github.com>
2024-04-20 11:54:34 +0800

    refactor: 略略略

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/DebugManage.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/DebugManage.kt
@@ -10,15 +10,14 @@ import net.ccbluex.liquidbounce.utils.ClientUtils
 
 @ModuleInfo(name = "DebugManage", description = "控制全局模块输出debugger", category = ModuleCategory.CLIENT, array = false)
 class DebugManage : Module() {
-    var prefixValue = BoolValue("Prefix",true)
-    var optValue = ListValue("Opt",arrayOf("All","Less"),"All")
-
     companion object {
+        var prefixValue = BoolValue("Prefix",true)
+        var optValue = ListValue("Opt",arrayOf("All","Less"),"All")
         @JvmStatic
         fun info(info: String) {
             if (
                 DarkNya.moduleManager[DebugManage::class.java].state  &&
-                (DarkNya.moduleManager[DebugManage::class.java] as DebugManage).optValue.get().toLowerCase() != "less"
+                optValue.get().toLowerCase() != "less"
             ) {
                 ClientUtils.displayChatMessage("${getPrefix()}§7$info")
             }
@@ -30,7 +29,7 @@ class DebugManage : Module() {
             }
         }
         private fun getPrefix(): String {
-            return if ((DarkNya.moduleManager[DebugManage::class.java] as DebugManage).prefixValue.get())
+            return if (prefixValue.get())
                 "§d${DarkNya.CLIENT_NAME} §8>> "
                 else ""
         }
