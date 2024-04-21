3a12a4a06f3dbbd2500475cbbd46922d3a8422cc
CatX_feitu <108512490+catx-feitu@users.noreply.github.com>
2024-04-21 13:38:02 +0800

    fix: EventManage关不掉

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/EventManage.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/client/EventManage.kt
@@ -1,5 +1,6 @@
 package net.ccbluex.liquidbounce.features.module.modules.client
 
+import net.ccbluex.liquidbounce.DarkNya
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
@@ -8,12 +9,14 @@ import net.ccbluex.liquidbounce.value.BoolValue
 @ModuleInfo(name = "EventManage", description = "事件管理", category = ModuleCategory.CLIENT, array = false)
 class EventManage : Module() {
     companion object {
-        var logExceptionInConsoleValue = BoolValue("LogExceptionInConsole",true)
-        var logExceptionInChatValue = BoolValue("LogExceptionInChat",true)
+        private var logExceptionInConsoleValue = BoolValue("LogExceptionInConsole",true)
+        private var logExceptionInChatValue = BoolValue("LogExceptionInChat",true)
         @JvmStatic
         fun onException(throwable: Throwable) {
+            if (!DarkNya.moduleManager[EventManage::class.java].state) return
+
             if (logExceptionInConsoleValue.get()) throwable.printStackTrace()
-            if (logExceptionInChatValue.get()) DebugManage.warn("在处理事件时发生异常:${throwable.javaClass.simpleName} ${throwable.message}")
+            if (logExceptionInChatValue.get()) DebugManage.warn("在处理事件时发生异常:${throwable.javaClass.simpleName}${if (throwable.message.isNullOrEmpty()) { "" } else { " ${throwable.message}" }}")
         }
     }
 }
