6620e7ec2b7cb91cbb1ac739a09dffa732b2b87e
CatX_feitu <108512490+catx-feitu@users.noreply.github.com>
2024-04-22 23:09:36 +0800

    更新(模块): TimeBalancer

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/TimeBalancer.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/world/TimeBalancer.kt
@@ -1,137 +1,83 @@
 package net.ccbluex.liquidbounce.features.module.modules.world
 
 import net.ccbluex.liquidbounce.event.EventTarget
-import net.ccbluex.liquidbounce.event.Render2DEvent
-import net.ccbluex.liquidbounce.event.TickEvent
+import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.event.UpdateEvent
+import net.ccbluex.liquidbounce.event.WorldEvent
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
-import net.ccbluex.liquidbounce.ui.cnfont.FontLoaders
-import net.ccbluex.liquidbounce.utils.render.RenderUtils
-import net.ccbluex.liquidbounce.utils.render.RoundedUtil
-import net.ccbluex.liquidbounce.value.*
 import net.ccbluex.liquidbounce.injection.implementations.IMixinTimer
-import net.minecraft.client.gui.ScaledResolution
-import net.minecraft.client.renderer.GlStateManager
-import java.awt.Color
+import net.ccbluex.liquidbounce.utils.MovementUtils
+import net.ccbluex.liquidbounce.value.BoolValue
+import net.ccbluex.liquidbounce.value.FloatValue
+import net.minecraft.network.play.client.*
+import net.minecraft.network.play.server.*
 
-@ModuleInfo(name = "TimerBalancer", description = "短时间内叠够足够的负数time后立刻释放", category = ModuleCategory.WORLD)
+
+@ModuleInfo(name = "TimeBalancer", description = "NoMove. NoCheck. NoProblem.", category = ModuleCategory.WORLD)
 class TimeBalancer : Module() {
-    /*
-    * 类似木糖醇的压缩timer，属于仿制品并且有瑕疵
-    * by Pursue(193923709)
-    * 开源地址：https://github.com/FKtzs/Nattalie
-    * 群号：673246810
-    */
-    private val timer = FloatValue("TimerSpeed", 4F, 0.1F, 10F) // Timer速度
-    private val time = IntegerValue("TimerMaxSet", 5, 0, 30) // 等待积攒时间
-    private val tie = IntegerValue("TimerSet", 5, 0, 30) // 释放时间
 
-    private val counter = BoolValue("Counter",true) // 渲染框架
-    private val setaX = ListValue("StartXMode", arrayOf("-","+"), "-")
-    private val xV = IntegerValue("StartX", 75, 0, 450)
-    private val setY = ListValue("StartYMode", arrayOf("-","+"), "+")
-    private val yV = IntegerValue("StartY", 20, 0, 450)
-
-    private val fontR = IntegerValue("FontR", 255, 0, 255)
-    private val fontG = IntegerValue("FontG", 255, 0, 255)
-    private val fontB = IntegerValue("FontB", 255, 0, 255)
-    private val fontA = IntegerValue("FontA", 255, 0, 255)
-
-    private val rectangleR = IntegerValue("RectangleR", 0, 0, 255)
-    private val rectangleG = IntegerValue("RectangleG", 0, 0, 255)
-    private val rectangleB = IntegerValue("RectangleB", 0, 0, 255)
-    private val rectangleA = IntegerValue("RectangleA", 125, 0, 255)
-
-    private val setFrame = ListValue("FrameMode", arrayOf("<","^"), "^")
-    private val frameR = IntegerValue("FrameR", 0, 0, 255)
-    private val frameG = IntegerValue("FrameG", 255, 0, 255)
-    private val frameB = IntegerValue("FrameB", 255, 0, 255)
-    private val frameA = IntegerValue("FrameA", 255, 0, 255)
-
-    private var lastMS = 0L
-    private var s = 0
-    private var m = 0
-    private var timr = false
-    private var progress = 0f
-
+    private val speedValue = FloatValue("Speed", 2F, 0.1F, 10F)
+    private val onlyOnGroundValue = BoolValue("OnlyOnGround", false)
+    private var currentTick = 0
 
     override fun onEnable() {
-        progress = 0f
+        (mc.timer as IMixinTimer).timerSpeed = 1F
+        currentTick = 0
     }
 
     override fun onDisable() {
-        s = 0
-        m = 0
-        timr = false
+        if (mc.player == null)
+            return
         (mc.timer as IMixinTimer).timerSpeed = 1F
+        currentTick = 0
     }
+
+    @EventTarget
+    fun onPacket(event: PacketEvent) {
+        when (event.packet) {
+            is CPacketKeepAlive,
+            is CPacketEntityAction,
+            is CPacketPlayerDigging,
+            is CPacketPlayerAbilities,
+            is CPacketClientStatus,
+            is SPacketAnimation,
+            is SPacketDisplayObjective,
+            is SPacketEntity,
+            is SPacketConfirmTransaction,
+            is SPacketPlayerAbilities,
+            is SPacketTimeUpdate -> event.cancelEvent()
+        }
+    }
+
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
-        if (timr) {
-            s++
-        } else {
-            m++
+        mc.player.isSprinting = false
+        if (onlyOnGroundValue.get() && !mc.player!!.onGround) return
+        if (currentTick < 0) currentTick = 0
+        if (currentTick > 1180) {
+            currentTick = 0
+            return
         }
-        if (m == time.get()) {
-            timr = true
-            (mc.timer as IMixinTimer).timerSpeed = timer.get()
-            m = 0
-            s = 0
+        if (!MovementUtils.isMoving) {
+            (mc.timer as IMixinTimer).timerSpeed = 1f
+            currentTick++
+            // 存储可加速tick
         }
-        if (s == tie.get()) {
-            timr = false
-            (mc.timer as IMixinTimer).timerSpeed = 1F
-            m = 0
-            s = 0
-        }
+        if (MovementUtils.isMoving && currentTick > 0) {
+            // 移动时每游戏刻减少 1t 并设置加速
+            currentTick -= speedValue.get().toInt() / 2 - 20
+            (mc.timer as IMixinTimer).timerSpeed = speedValue.get()
+        } else (mc.timer as IMixinTimer).timerSpeed = 1f
     }
 
     @EventTarget
-    fun onTick(event: TickEvent?) {
-        val timer = mc.timer ?: return
-        try {
-            val f = timer.javaClass.getDeclaredField("field_74277_g")
-            f.setAccessible(true)
-            val t = f[timer] as Long
-            f[timer] = t + 50L
-        } catch (e: NoSuchFieldException) {
-            e.printStackTrace()
-        } catch (e: IllegalAccessException) {
-            e.printStackTrace()
-        }
+    fun onWorld(event: WorldEvent) {
+        // 如果世界客户端为空，则禁用模块
+        if (event.worldClient != null)
+            state = false
     }
-
-    @EventTarget
-    fun onRender2D(event: Render2DEvent) {
-        progress = (System.currentTimeMillis() - lastMS).toFloat() / 100f
-        if (progress >= 1) progress = 1f
-        val scaledResolution = ScaledResolution(mc)
-        val counterMode = counter.get()
-        val startX = if (setaX.get() == "-") scaledResolution.scaledWidth / 2 - xV.get() else scaledResolution.scaledWidth / 2 + xV.get()
-        val startY = if (setY.get() == "-") scaledResolution.scaledHeight / 2 - yV.get() else scaledResolution.scaledHeight / 2 + yV.get()
-
-        if (counterMode) {
-            RenderUtils.drawShadow(startX, startY, 160, 21)
-            GlStateManager.resetColor()
-
-            RoundedUtil.drawRound(startX.toFloat(), startY.toFloat(), 160f, 22f, 3F, Color(rectangleR.get(), rectangleG.get(), rectangleB.get(), rectangleA.get()))
-            when (setFrame.get().toLowerCase()) {
-                "^" -> {
-                    RoundedUtil.drawRound(startX.toFloat(), startY.toFloat(), 160f, 3f, 3F, Color(frameR.get(), frameG.get(),frameB.get(), frameA.get()))
-                    GlStateManager.resetColor()
-                    FontLoaders.F24.drawString("Timer：$m ReleaseTime：$s", (startX - 4 + 36).toFloat(), (startY + 7.5).toFloat(), Color(fontR.get(),fontG.get(),fontB.get(),fontA.get()).rgb)
-                    GlStateManager.resetColor()
-                }
-                "<" -> {
-                    RoundedUtil.drawRound(startX.toFloat(), startY.toFloat(), 3f, 22f, 3F, Color(frameR.get(), frameG.get(),frameB.get(), frameA.get()))
-                    GlStateManager.resetColor()
-                    FontLoaders.F24.drawString("Timer：$m ReleaseTime：$s", (startX - 4 + 36).toFloat(), (startY + 6.5).toFloat(), Color(fontR.get(),fontG.get(),fontB.get(),fontA.get()).rgb)
-                    GlStateManager.resetColor()
-                }
-            }
-            GlStateManager.resetColor()
-        }
-    }
+    override val tag: String
+        get() = "AccelerationTime: $currentTick" + "ticks"
 }
