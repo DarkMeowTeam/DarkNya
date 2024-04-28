c6c2b3c7acaa44080b5d6b1804d9d501cd68f040
CatX_feitu <108512490+catx-feitu@users.noreply.github.com>
2024-04-28 15:26:56 +0800

    重写: Value

--- src/main/java/net/ccbluex/liquidbounce/value/Value.kt
+++ src/main/java/net/ccbluex/liquidbounce/value/Value.kt
@@ -1,32 +1,28 @@
 package net.ccbluex.liquidbounce.value
 
 import com.google.gson.JsonElement
-import com.google.gson.JsonObject
 import com.google.gson.JsonPrimitive
 import net.ccbluex.liquidbounce.DarkNya
-import net.ccbluex.liquidbounce.ui.font.Fonts
 import net.ccbluex.liquidbounce.utils.ClientUtils
-import net.minecraft.client.gui.FontRenderer
-import java.util.*
+import java.awt.Color
+import kotlin.jvm.internal.Intrinsics
 
 abstract class Value<T>(val name: String, var value: T) {
+    val default = value
+    var textHovered: Boolean = false
 
-    private var displayableFunc: () -> Boolean = { true }
+    private var displayableFunc: MutableList<() -> Boolean> = mutableListOf()
 
     fun displayable(func: () -> Boolean): Value<T> {
-        displayableFunc = func
+        displayableFunc.add(func)
         return this
     }
 
     val displayable: Boolean
-        get() = displayableFunc()
-
-    val displayableFunction: () -> Boolean
-        get() = displayableFunc
+        get() = displayableFunc.all { it.invoke() }
 
     fun set(newValue: T) {
-        if (newValue == value)
-            return
+        if (newValue == value) return
 
         val oldValue = get()
 
@@ -42,6 +38,10 @@ abstract class Value<T>(val name: String, var value: T) {
 
     fun get() = value
 
+    fun setDefault() {
+        value = default
+    }
+
     open fun changeValue(value: T) {
         this.value = value
     }
@@ -52,128 +52,56 @@ abstract class Value<T>(val name: String, var value: T) {
     protected open fun onChange(oldValue: T, newValue: T) {}
     protected open fun onChanged(oldValue: T, newValue: T) {}
 
-}
-
-/**
- * Bool value represents a value with a boolean
- */
-open class BoolValue(name: String, value: Boolean) : Value<Boolean>(name, value) {
-
-    override fun toJson() = JsonPrimitive(value)
-
-    override fun fromJson(element: JsonElement) {
-        if (element.isJsonPrimitive)
-            value = element.asBoolean || element.asString.equals("true", ignoreCase = true)
-    }
-
-}
-
-/**
- * Integer value represents a value with a integer
- */
-open class IntegerValue(name: String, value: Int, val minimum: Int = 0, val maximum: Int = Integer.MAX_VALUE)
-    : Value<Int>(name, value) {
-
-    fun set(newValue: Number) {
-        set(newValue.toInt())
-    }
-
-    override fun toJson() = JsonPrimitive(value)
-
-    override fun fromJson(element: JsonElement) {
-        if (element.isJsonPrimitive)
-            value = element.asInt
-    }
-
-}
-
-/**
- * Float value represents a value with a float
- */
-open class FloatValue(name: String, value: Float, val minimum: Float = 0F, val maximum: Float = Float.MAX_VALUE)
-    : Value<Float>(name, value) {
-
-    fun set(newValue: Number) {
-        set(newValue.toFloat())
-    }
-
-    override fun toJson() = JsonPrimitive(value)
-
-    override fun fromJson(element: JsonElement) {
-        if (element.isJsonPrimitive)
-            value = element.asFloat
-    }
-
-}
-
-/**
- * Text value represents a value with a string
- */
-open class TextValue(name: String, value: String) : Value<String>(name, value) {
-
-    override fun toJson() = JsonPrimitive(value)
-
-    override fun fromJson(element: JsonElement) {
-        if (element.isJsonPrimitive)
-            value = element.asString
-    }
-}
-
-/**
- * Font value represents a value with a font
- */
-class FontValue(valueName: String, value: FontRenderer) : Value<FontRenderer>(valueName, value) {
-
-    override fun toJson(): JsonElement? {
-        val fontDetails = Fonts.getFontDetails(value) ?: return null
-        val valueObject = JsonObject()
-        valueObject.addProperty("fontName", fontDetails.name)
-        valueObject.addProperty("fontSize", fontDetails.fontSize)
-        return valueObject
-    }
-
-    override fun fromJson(element: JsonElement) {
-        if (!element.isJsonObject) return
-        val valueObject = element.asJsonObject
-        value = Fonts.getFontRenderer(valueObject["fontName"].asString, valueObject["fontSize"].asInt)
-    }
-}
-
-/**
- * Block value represents a value with a block
- */
-class BlockValue(name: String, value: Int) : IntegerValue(name, value, 1, 197)
-
-/**
- * List value represents a selectable list of values
- */
-open class ListValue(name: String, val values: Array<String>, value: String) : Value<String>(name, value) {
-
-    @JvmField
-    var openList = false
-
-    init {
-        this.value = value
-    }
-
-    operator fun contains(string: String?): Boolean {
-        return Arrays.stream(values).anyMatch { s: String -> s.equals(string, ignoreCase = true) }
-    }
-
-    override fun changeValue(value: String) {
-        for (element in values) {
-            if (element.equals(value, ignoreCase = true)) {
-                this.value = element
-                break
-            }
+    open class ColorValue(name: String, value: Int, canDisplay: () -> Boolean) : Value<Int>(name, value) {
+        val minimum: Int = -10000000
+        val maximum: Int = 1000000
+        fun set(newValue: Number) {
+            set(newValue.toInt())
+        }
+        override fun toJson() = JsonPrimitive(value)
+        override fun fromJson(element: JsonElement) {
+            if (element.isJsonPrimitive)
+                value = element.asInt
         }
     }
 
-    override fun toJson() = JsonPrimitive(value)
+    override fun equals(other: Any?): Boolean {
+        other ?: return false
+        if (value is String && other is String) {
+            return (value as String).equals(other, true)
+        }
+        return value?.equals(other) ?: false
+    }
 
-    override fun fromJson(element: JsonElement) {
-        if (element.isJsonPrimitive) changeValue(element.asString)
+    fun contains(text: String/*, ignoreCase: Boolean*/): Boolean {
+        return if (value is String) {
+            (value as String).contains(text, true)
+        } else {
+            false
+        }
+    }
+
+    private var Expanded = false
+
+    open fun getExpanded(): Boolean {
+        return Expanded
+    }
+
+    open fun setExpanded(b: Boolean) {
+        this.Expanded
+    }
+
+    open fun isExpanded(): Boolean {
+        return Expanded
     }
 
 
+    open fun getAwtColor(): Color {
+        return Color((this as Value<Number>).value.toInt(), true)
+    }
+
+    open fun ColorValue(name: String, value: Int) {
+        Intrinsics.checkParameterIsNotNull(name, "name")
+        ColorValue(name, value)
+    }
 }
