ee49b1605c89082d4ecc68bb903a3f7d802f4509
CatX_feitu <108512490+catx-feitu@users.noreply.github.com>
2024-05-05 20:28:41 +0800

    新增(NameTags): rect改为两行 分辨显示原本血量和伤害吸收
    并微调GL使用
    和允许关闭rect血量显示功能

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/NameTags.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/NameTags.kt
@@ -1,6 +1,5 @@
 package net.ccbluex.liquidbounce.features.module.modules.render
 
-import net.ccbluex.liquidbounce.DarkNya
 import net.ccbluex.liquidbounce.event.EventTarget
 import net.ccbluex.liquidbounce.event.Render3DEvent
 import net.ccbluex.liquidbounce.features.module.Module
@@ -21,9 +20,9 @@ import net.ccbluex.liquidbounce.value.BoolValue
 import net.ccbluex.liquidbounce.value.FloatValue
 import net.ccbluex.liquidbounce.value.FontValue
 import net.ccbluex.liquidbounce.value.ListValue
+import net.minecraft.client.renderer.GlStateManager.*
 import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.potion.PotionUtils
 import org.lwjgl.opengl.GL11.*
 import java.awt.Color
 import kotlin.math.roundToInt
@@ -40,11 +39,23 @@ class NameTags : Module() {
     private val fontValue = FontValue("Font", Fonts.minecraftFont)
 
     private val rectValue = ListValue("Rect", arrayOf("None","Up","Down"),"Up")
+    private val rectDisplayHealthValue = BoolValue("RectDisplayHealth", true).displayable { rectValue.get().toLowerCase() != "none" }
 
     private val scaleValue = FloatValue("Scale", 1F, 1F, 4F)
 
     @EventTarget
     fun onRender3D(event: Render3DEvent) {
+        glPushAttrib(GL_ENABLE_BIT)
+        glPushMatrix()
+
+        glDisable(GL_LIGHTING)
+        glDisable(GL_DEPTH_TEST)
+
+        glEnable(GL_LINE_SMOOTH)
+
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
         for (entity in mc.world!!.loadedEntityList) {
             if (!EntityUtils.isSelected(entity, false))
                 continue
@@ -58,7 +69,12 @@ class NameTags : Module() {
                     (entity.displayName ?: continue).unformattedText
             )
         }
-        if (selfValue.get()) renderNameTag(mc.player,mc.player.name)
+        if (selfValue.get() && mc.gameSettings.thirdPersonView != 0) renderNameTag(mc.player,mc.player.name)
+
+        glPopMatrix()
+        glPopAttrib()
+
+        glColor4f(1F, 1F, 1F, 1F)
     }
 
     private fun renderNameTag(entity: EntityLivingBase, tag: String) {
@@ -119,48 +135,81 @@ class NameTags : Module() {
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
 
-        var health = entity.health / entity.maxHealth
-        if (health > 1F) health = 1F // 生命附加之类的BUFF血量大于100%
+        // rect 第一行 基本血量
+        var health = if (rectDisplayHealthValue.get()) { entity.health / entity.maxHealth } else { 1F }
+        // rect 第二行 伤害吸收额外血量
+        var healthExpand = if (rectDisplayHealthValue.get()) { entity.absorptionAmount / entity.maxHealth } else { 0F }
+
+        // 避免血量画出框
+        if (health > 1F) health = 1F
+        if (healthExpand > 1F) healthExpand = 1F
 
         val healthDrawX = -width - 2F
         val healthDrawXEnd = healthDrawX + (health * ( width * 2 + 5F))
-
+        val healthDrawXEndExpand = healthDrawX + (healthExpand * ( width * 2 + 5F))
         glDisable(GL_TEXTURE_2D)
         glEnable(GL_BLEND)
 
         quickDrawRect(-width - 2F, -2F, width + 3F, fontRenderer.FONT_HEIGHT + 3F, Integer.MIN_VALUE)
 
-        // 默认NameTags颜色
+        // 默认rect1颜色
         var color = ColorManage.getColorByTime()
         try {
             if (entity is EntityPlayer) {
-                // 生命恢复黄色NameTags颜色
-                if (DarkNyaPotionUtils.getRegenerationEffectDuration(entity) != 0) color = Color(240, 240, 40).rgb
                 // 力量效果红色NameTags颜色
                 if (DarkNyaPotionUtils.getStrengthEffectDuration(entity) != 0) color = Color(240, 40, 40).rgb
                 // 高风险用户橙色NameTags颜色
                 if (HighDamageDetector.displayInNameTagsValue.get() && HighDamageDetector.warnPlayers.contains(entity)) color = Color(200, 100, 0).rgb
             }
         } catch (_:Exception) { }
+        // 默认rect2颜色
+        var colorExpand = Color(240, 240, 40).rgb
+        try {
+            if (entity is EntityPlayer) {
+                // 生命恢复 - 红色
+                if (DarkNyaPotionUtils.getRegenerationEffectDuration(entity) != 0) colorExpand = Color(200, 100, 0).rgb
+            }
+        } catch (_:Exception) { }
         when (rectValue.get().toLowerCase()) {
-            "up" -> quickDrawRect(
-                healthDrawX,
-                -3F,
-                healthDrawXEnd,
-                -4F,
-                color
-            )
-            "down" -> quickDrawRect(
-                healthDrawX,
-                fontRenderer.FONT_HEIGHT + 2F,
-                healthDrawXEnd,
-                fontRenderer.FONT_HEIGHT + 3F,
-                color
-            )
+            "up" -> {
+                // rect1
+                quickDrawRect(
+                    healthDrawX,
+                    -3F,
+                    healthDrawXEnd,
+                    -4F,
+                    color
+                )
+                // rect2
+                quickDrawRect(
+                    healthDrawX,
+                    -2F,
+                    healthDrawXEndExpand,
+                    -3F,
+                    colorExpand
+                )
+            }
+            "down" -> {
+                // rect1
+                quickDrawRect(
+                    healthDrawX,
+                    fontRenderer.FONT_HEIGHT + 2F,
+                    healthDrawXEnd,
+                    fontRenderer.FONT_HEIGHT + 3F,
+                    color
+                )
+                // rect2
+                quickDrawRect(
+                    healthDrawX,
+                    fontRenderer.FONT_HEIGHT + 1F,
+                    healthDrawXEndExpand,
+                    fontRenderer.FONT_HEIGHT + 2F,
+                    colorExpand
+                )
+            }
         }
 
         glEnable(GL_TEXTURE_2D)
-        glDisable(GL_BLEND)
 
 
         fontRenderer.drawString(text, 1F + -width, if (fontRenderer == Fonts.minecraftFont) 1F else 1.5F,
@@ -169,7 +218,8 @@ class NameTags : Module() {
         AWTFontRenderer.assumeNonVolatile = false
 
         RenderUtils.resetCaps()
-
+        glColor4f(1F, 1F, 1F, 1F)
+        resetColor()
         // Pop
         glPopMatrix()
     }
